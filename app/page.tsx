5f21652285701f61737d3f5f532b76a567bce2c0
AAVN\thkha <kha.tranhuynh@axonactive.com>
2023-03-21 16:52:23 +0700

    Install and apply tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <Image
-      src="/wallpaper.jpg"
-      alt="Picture of the author"
-      width="0"
-      height="0"
-      sizes="100vw"
-      className="w-full h-auto"
-    />
+    // <Image
+    //   src="/wallpaper.jpg"
+    //   alt="Picture of the author"
+    //   width="0"
+    //   height="0"
+    //   sizes="100vw"
+    //   className="w-full h-auto"
+    // />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
