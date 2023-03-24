fe916d1514cd88d390a028827a48a1335d209fd9
AAVN\thkha <kha.tranhuynh@axonactive.com>
2023-03-24 15:01:33 +0700

    First step home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
+'use client';
+import { Box } from '@mui/material';
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    // <Image
-    //   src="/wallpaper.jpg"
-    //   alt="Picture of the author"
-    //   width="0"
-    //   height="0"
-    //   sizes="100vw"
-    //   className="w-full h-auto"
-    // />
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+	return (
+		<div>
+			<div>
+				<Image
+					src="/wallpaper.jpg"
+					alt="Picture of the author"
+					width="0"
+					height="0"
+					sizes="100vw"
+					className="w-full h-auto"
+				/>
+			</div>
+			<div className="limited-width-layout__content">
+				<Box sx={{ display: 'flex' }}>
+					<Box sx={{ display: 'flex', flexDirection: 'column', width: '33%' }}>
+						<label>INSPIRATION</label>
+						<text>Discover incredible destinations, exceptional accommodations, and exciting experiences</text>
+					</Box>
+					<Box sx={{ display: 'flex', flexDirection: 'column', width: '33%' }}>
+						<label>INSPIRATION</label>
+						<text>Discover incredible destinations, exceptional accommodations, and exciting experiences</text>
+					</Box>
+					<Box sx={{ display: 'flex', flexDirection: 'column', width: '33%' }}>
+						<label>INSPIRATION</label>
+						<text>Discover incredible destinations, exceptional accommodations, and exciting experiences</text>
+					</Box>
+				</Box>
+			</div>
+		</div>
+	)
 }
