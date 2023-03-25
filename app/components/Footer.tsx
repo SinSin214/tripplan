2639435e0ce1565bb973ddab9b2a70efbebe39f0
Huynh Kha <tranhuynhkha21496@gmail.com>
2023-03-26 01:55:24 +0700

    Adjust css

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,24 +3,24 @@ import { VerticalLine } from "./SmallThings";
 
 export default function Footer() {
     return (
-        <Box className="bg-stone-800 text-white py-8">
+        <Box className="bg-stone-800 text-white py-10">
             <Box className="flex limited-width-layout__content justify-between">
                 <Box className="flex flex-col w-full" >
-                    <h3>Contact</h3>
+                    <label>CONTACT</label>
                     <text>Email: tranhuynhkha21496@gmail.com</text>
                     <text>Skype: live:tranhuynhkha21496</text>
                     <text>Github: github.com/SinSin214</text>
                 </Box>
                 <VerticalLine />
                 <Box className="flex flex-col w-full" >
-                    <h3>Contact</h3>
+                    <label>CONTACT</label>
                     <text>Email: tranhuynhkha21496@gmail.com</text>
                     <text>Skype: live:tranhuynhkha21496</text>
                     <text>Github: github.com/SinSin214</text>
                 </Box>
                 <VerticalLine />
                 <Box className="flex flex-col w-full" >
-                    <h3>Contact</h3>
+                    <label>CONTACT</label>
                     <text>Email: tranhuynhkha21496@gmail.com</text>
                     <text>Skype: live:tranhuynhkha21496</text>
                     <text>Github: github.com/SinSin214</text>
