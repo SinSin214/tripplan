1cf1a80575a466ee77362cc4735db39e219f1197
AAVN\thkha <kha.tranhuynh@axonactive.com>
2023-09-06 16:11:48 +0700

    Ignore folder dist

--- server/dist/src/post/post.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { PostService } from './post.service';
-export declare class PostController {
-    private postService;
-    constructor(postService: PostService);
-    getAll(): Promise<any[]>;
-    getDetail(id: string): Promise<Object>;
-}
--- server/dist/src/post/post.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { MiddlewareConsumer } from '@nestjs/common';
-export declare class PostModule {
-    configure(consumer: MiddlewareConsumer): void;
-}
